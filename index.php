382517d946ab0ea553514b0610c6d064bc20ce43
Alfredo Stochiero <alfredo_stochiero@hotmail.com>
2020-01-17 20:59:13 -0300

    Funcao que retorna imagem e funcao que retorna duracao do video

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 require 'simple_html_dom.php';
 
+$subject = 'snes';
+
+$html = file_get_html('https://www.youtube.com/results?search_query='.$subject);
+
+$results = $html->find('.yt-lockup');  
+
+foreach ($results as $video) {
+	$image = $video->find('.yt-thumb-simple img',0)->attr['src'];// find the video's image source link
+	$time = $video->find('.video-time',0)->plaintext; // find the duration of the video
+
+	echo "Image: ".$image."<br/>";
+	echo "Time: ".$time."<br/>";
+	echo "<hr/>";
+}
+
+
+
+
+
 
 ?>
