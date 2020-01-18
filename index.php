994b70e777b3f3537fd4274284d94c05e0fd89b5
Alfredo Stochiero <alfredo_stochiero@hotmail.com>
2020-01-17 21:53:11 -0300

    tabela criada para melhor exibicao de dados

--- index.php
+++ index.php
@@ -1,34 +1,47 @@
+<table>
+	<tr>
+		<th>Image</th>
+		<th>Título</th>
+		<th>Autor</th>
+		<th>Link</th>
+		<th>Duração</th>
+		<th>Views</th>
+	</tr>
+
 <?php
 require 'simple_html_dom.php';
 
-$subject = 'nes';
+$subject = 'megaman';
 
 $html = file_get_html('https://www.youtube.com/results?search_query='.$subject);
 
 $results = $html->find('.yt-lockup');  
 
 foreach ($results as $video) {
+	// All variables are in the String format
 	$image = $video->find('.yt-thumb-simple img',0)->attr['src'];// find the video's image source link
 	$time = $video->find('.video-time',0)->plaintext; // find the duration of the video
 	$link = $video->find('.yt-lockup-title a',0)->href;
-	$linkCompleto = 'https://www.youtube.com'.$link;
+	$completeLink = 'https://www.youtube.com'.$link;
 	$title = $video->find('.yt-lockup-title a',0)->plaintext; // it gets the title of video 
 	$author = $video->find('.yt-lockup-byline a',0)->plaintext; // it gets the person that uploaded the video
 	$views = $video->find('.yt-lockup-meta-info li',1)->plaintext;
 
-	// All variables are in the String format
+	?>
 
-	echo "Image: ".$image."<br/>";
-	echo "Time: ".$time."<br/>";
-	echo "Link :".$linkCompleto.'<br/>';
-	echo "Title :".$title.'<br/>';
-	echo "Author :".$author.'<br/>';
-	echo "Views :".$views."<br/>";
-
-	echo "<hr/>";
+	<tr>
+		<td> <img src="<?php echo $image; ?>" /> </td>
+		<td><?php echo $title; ?>		</td>
+		<td><?php echo $author; ?>		</td>
+		<td><?php echo $completeLink; ?></td>
+		<td><?php echo $time; ?>		</td>
+		<td><?php echo $views; ?>		</td>
+	</tr>
+    <?php
+	
 }
-
-
+?>
+</table>
 
 
 
